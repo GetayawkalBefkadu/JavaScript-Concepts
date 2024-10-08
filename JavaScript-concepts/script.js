b0a5d94802f234a045df4fb750f2ca60ea716c70
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-08 09:43:52 -0900

    Update script.js

--- JavaScript-concepts/script.js
+++ JavaScript-concepts/script.js
@@ -1,3 +1,8 @@
 function sayHello() {
   alert('Hello from script.js!');
 }
+let name = 'John'; // String
+const age = 30; // Number
+var isStudent = true; // Boolean
+
+console.log(name, age, isStudent);
