fa60bb5b4ee97b2644e02a6462b5846da7280ae9
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-08 09:51:46 -0900

    Update script.js

--- JavaScript-concepts/script.js
+++ JavaScript-concepts/script.js
@@ -16,3 +16,13 @@ greet(); // Calling the function
 for (let i = 0; i < 5; i++) {
   console.log(`Iteration ${i}`);
 }
+
+let score = 85;
+
+if (score >= 90) {
+  console.log('A');
+} else if (score >= 80) {
+  console.log('B');
+} else {
+  console.log('C');
+}
