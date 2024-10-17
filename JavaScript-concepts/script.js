15451c2769f6b0558273b3ac5d2a0a22bf137905
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-17 09:06:23 -0900

    Update script.js

--- JavaScript-concepts/script.js
+++ JavaScript-concepts/script.js
@@ -57,3 +57,17 @@ function checkScope() {
 }
 
 checkScope();
+
+
+
+const bookSeeker = "James";
+const book = "Great Expectations";
+function library() {
+  const librarian = "Julia"; 
+    console.log(bookSeeker + " asked " + librarian + " for " + book);
+    function classicLiterature() {
+        const shelf = "Dickens"; 
+        console.log( bookSeeker + " found " + book + " on the " + shelf + " shelf!");
+    }
+    classicLiterature();
+}
