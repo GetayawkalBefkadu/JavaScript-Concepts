4ac672b5ca82585c2379a1165ae20597ba00d6db
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-22 09:28:30 -0900

    Update script.js

--- JavaScript-concepts/script.js
+++ JavaScript-concepts/script.js
@@ -1,117 +1,161 @@
-// this are just the basics of javascript concepts and it will be updated and applay them ont the real projects!!
-// for now see the basics and applay them on your consol. and trust the process 
-// try the script or the function separetly in ordoer to see the specfic result
-// Thank You!!!
-
-function sayHello() {
-  alert('Hello from script.js!');
-}
-let name = 'John'; // String
-const age = 30; // Number
-var isStudent = true; // Boolean
-
-console.log(name, age, isStudent);
-
-function greet() {
-  console.log('Hello, there!');
-}
-
-greet(); // Calling the function
-
-for (let i = 0; i < 5; i++) {
-  console.log(`Iteration ${i}`);
-}
-
-let score = 85;
-
-if (score >= 90) {
-  console.log('A');
-} else if (score >= 80) {
-  console.log('B');
-} else {
-  console.log('C');
-}
-
-
-
-var greeting = "Hello";
-let name = "Alice";
-
-const age = 25;
-
-console.log(greeting); // Output: Hello
-console.log(name);     // Output: Alice
-console.log(age);      // Output: 25
+let name = "Alice"; // Mutable variable
+const age = 25;    // Immutable variable
 
 
 
 
-function checkScope() {
-  if (true) {
-    var functionScope = 'I am visible within the function';
-    let blockScope = 'I am visible only in this block';
-    const anotherBlockScope = 'I am also visible only in this block';
-  }
-  console.log(functionScope); // Output: I am visible within the function
-  console.log(blockScope);    // Error: blockScope is not defined
-  console.log(anotherBlockScope); // Error: anotherBlockScope is not defined
-}
-
-checkScope();
 
 
 
-const bookSeeker = "James";
-const book = "Great Expectations";
-function library() {
-  const librarian = "Julia"; 
-    console.log(bookSeeker + " asked " + librarian + " for " + book);
-    function classicLiterature() {
-        const shelf = "Dickens"; 
-        console.log( bookSeeker + " found " + book + " on the " + shelf + " shelf!");
-    }
-    classicLiterature();
-}
 
 
 
-let fruit = "apple"; 
-
-if (fruit !== "banana") {
-    let fruit = "banana";
-    console.log(fruit);   
-}
-console.log(fruit);  
 
 
 
-let x = 1;
-
-function addTwo() {
-    x = x + 2;
-}
-
-addTwo();
-x = x + 1;
-console.log(x);
 
 
 
-const catSays = function(max) {
-  let catMessage = "";
-  for (let i = 0; i < max; i++) {
-    catMessage += "meow ";
-  }
-  return catMessage;
-};
 
-catSays;
-// --------------------------------------------
-function makeLine(length) {
-  var let = "";
-  for (let j = 1; j <= length; j++) {
-    line += "* "
-  }
-  return line + "\n";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // this are just the basics of javascript concepts and it will be updated and applay them ont the real projects!!
+// // for now see the basics and applay them on your consol. and trust the process 
+// // try the script or the function separetly in ordoer to see the specfic result
+// // Thank You!!!
+
+// function sayHello() {
+//   alert('Hello from script.js!');
+// }
+// let name = 'John'; // String
+// const age = 30; // Number
+// var isStudent = true; // Boolean
+
+// console.log(name, age, isStudent);
+
+// function greet() {
+//   console.log('Hello, there!');
+// }
+
+// greet(); // Calling the function
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(`Iteration ${i}`);
+// }
+
+// let score = 85;
+
+// if (score >= 90) {
+//   console.log('A');
+// } else if (score >= 80) {
+//   console.log('B');
+// } else {
+//   console.log('C');
+// }
+
+
+
+// var greeting = "Hello";
+// let name = "Alice";
+
+// const age = 25;
+
+// console.log(greeting); // Output: Hello
+// console.log(name);     // Output: Alice
+// console.log(age);      // Output: 25
+
+
+
+
+// function checkScope() {
+//   if (true) {
+//     var functionScope = 'I am visible within the function';
+//     let blockScope = 'I am visible only in this block';
+//     const anotherBlockScope = 'I am also visible only in this block';
+//   }
+//   console.log(functionScope); // Output: I am visible within the function
+//   console.log(blockScope);    // Error: blockScope is not defined
+//   console.log(anotherBlockScope); // Error: anotherBlockScope is not defined
+// }
+
+// checkScope();
+
+
+
+// const bookSeeker = "James";
+// const book = "Great Expectations";
+// function library() {
+//   const librarian = "Julia"; 
+//     console.log(bookSeeker + " asked " + librarian + " for " + book);
+//     function classicLiterature() {
+//         const shelf = "Dickens"; 
+//         console.log( bookSeeker + " found " + book + " on the " + shelf + " shelf!");
+//     }
+//     classicLiterature();
+// }
+
+
+
+// let fruit = "apple"; 
+
+// if (fruit !== "banana") {
+//     let fruit = "banana";
+//     console.log(fruit);   
+// }
+// console.log(fruit);  
+
+
+
+// let x = 1;
+
+// function addTwo() {
+//     x = x + 2;
+// }
+
+// addTwo();
+// x = x + 1;
+// console.log(x);
+
+
+
+// const catSays = function(max) {
+//   let catMessage = "";
+//   for (let i = 0; i < max; i++) {
+//     catMessage += "meow ";
+//   }
+//   return catMessage;
+// };
+
+// catSays;
+// // --------------------------------------------
+// function makeLine(length) {
+//   var let = "";
+//   for (let j = 1; j <= length; j++) {
+//     line += "* "
+//   }
+//   return line + "\n";
+// }
 
