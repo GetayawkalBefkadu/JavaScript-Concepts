8a199e95919db4db6372ce20b08efcc4b229c0ad
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-23 09:11:41 -0900

    Update script.js

--- JavaScript-concepts/script.js
+++ JavaScript-concepts/script.js
@@ -127,6 +127,13 @@ export function add(a, b) {
 }
 
 //----------------------------------------------
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5]; // Outputs: [1, 2, 3, 4, 5]
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log(sum(1, 2, 3, 4)); // Output: 10
 
 //----------------------------------------------
 //----------------------------------------------
