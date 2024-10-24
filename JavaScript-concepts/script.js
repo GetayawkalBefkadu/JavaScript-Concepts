e2e2dfcdf1950424307f221facbd4d4d46716801
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-24 07:31:12 -0900

    Update script.js

--- JavaScript-concepts/script.js
+++ JavaScript-concepts/script.js
@@ -172,6 +172,11 @@ const facebookProfile = {
     }
 };
 //----------------------------------------------
+donuts.forEach(function(donut){
+    // donut represents a single element of donuts array
+    // donut is an object, therefore you can access its properties using a dot notation
+    console.log(donut.type+" donuts cost $"+donut.cost+" each");
+});
 //----------------------------------------------
 //----------------------------------------------
 //----------------------------------------------
